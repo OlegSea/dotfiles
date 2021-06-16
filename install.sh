ba3efae74070c04dea6e5b8cd710ed4ca99fe328
OlegSea <ta.alexashow@ya.ru>
2021-06-16 23:37:10 +0300

    First commit

---
+++ install.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+path=$(pwd)
+
+sudo pacman -S --needed xorg git base-devel wget bspwm sxhkd zsh pulseaudio pulseaudio-alsa pavucontrol python python-dbus python-pip rofi kitty scrot xclip
+
+sudo pacman -S --needed git base-devel
+git clone https://aur.archlinux.org/yay.git
+cd yay
+makepkg -si
+cd ..
+rm -rf yay
+
+[ ! -d $HOME/.config ] && mkdir $HOME/.config
+cp -rf config/* $HOME/.config/
+cp -rf scripts/ $HOME/scripts/
