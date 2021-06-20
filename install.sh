e1c59a958bfbfae30a3789b0a7aae5475f74baae
olegsea <ta.alexashow@ya.ru>
2021-06-20 10:36:15 +0300

    a lot of stuff

--- install.sh
+++ install.sh
@@ -1,9 +1,10 @@
 #!/bin/sh
 path=$(pwd)
 
-sudo pacman -S --needed xorg git base-devel wget bspwm sxhkd zsh pulseaudio pulseaudio-alsa pavucontrol python python-dbus python-pip rofi kitty scrot xclip ntfs-3g picom hsetroot ttf-font-awesome awesome-terminal-fonts noto-fonts-cjk
+sudo cp pacman.conf /etc/
 
-sudo pacman -S --needed git base-devel
+sudo pacman -Sy --needed xorg git base-devel wget bspwm sxhkd zsh dunst pulseaudio pulseaudio-alsa pavucontrol python python-dbus python-pip rofi kitty scrot xclip ntfs-3g picom hsetroot ttf-font-awesome awesome-terminal-fonts noto-fonts-cjk mpv thunar nomacs 
+
 git clone https://aur.archlinux.org/yay.git
 cd yay
 makepkg -si
@@ -15,5 +16,7 @@ cp -rf config/* $HOME/.config/
 cp -rf scripts/ $HOME/scripts/
 cp .zshrc $HOME/
 sudo cp -rf config-other/picom.conf /etc/xdg/picom.conf
+
+yay -S --needed - < yay-packages.txt
 
 sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
